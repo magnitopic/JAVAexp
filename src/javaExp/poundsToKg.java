9889c0d8f9b19699a892fad1f9bb01e9cbbc5ccc
magnitopic <magnitopic@gmail.com>
2021-06-19 01:03:35 +0200

    poundsToKg

---
+++ src/javaExp/poundsToKg.java
@@ -0,0 +1,9 @@
+package javaExp;
+
+public class poundsToKg {
+	public static void main(String[] args) {
+		double pounds=200d;
+		double result=pounds*0.453592;
+		System.out.println(pounds+" pounds is equal to "+result+" Kg");
+	}
+}
