7dc6f5c7b0b2c3fbb5ebdcc1ab513e63e4a7eea2
magnitopic <magnitopic@gmail.com>
2021-07-02 00:56:41 +0200

    BarkingDog

--- src/javaExp/RockPaperScissors.java
+++ src/javaExp/RockPaperScissors.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 public class RockPaperScissors {
 	public static void main(String[] args){
 		
-		
-		do {
-			String userR = user();
-		};
-		while (!userR.equals("1") && !userR.equals("2") && !userR.equals("3") && !userR.equals("r") && !userR.equals("p") && !userR.equals("s") && !userR.equals("rock") && !userR.equals("paper") && !userR.equals("scissors"));
-		
+		do{
+			Example.AnsUser=user();
+		}
+		while (Example.AnsUser!="mag");
+
+	}
+	public abstract class Example {
+		public static final String AnsUser;
 	}
 	public static String user() {
 		Scanner myObj = new Scanner(System.in);
