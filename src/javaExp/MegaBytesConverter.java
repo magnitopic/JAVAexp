2c278601eabb8182027ff841c964b14adbd79d1e
magnitopic <magnitopic@gmail.com>
2021-07-01 23:34:23 +0200

    Exercice version of megabytes

--- src/javaExp/MegaBytesConverter.java
+++ src/javaExp/MegaBytesConverter.java
@@ -7,8 +7,12 @@ public class MegaBytesConverter {
     }
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes){
-        int megas=kiloBytes/1024;
-        int KBrestantes=-((megas*1024)-kiloBytes);
-        System.out.println(kiloBytes+"KB = "+megas+"MB and "+KBrestantes+"KB");
+        if (kiloBytes<0){
+			System.out.print("Invalid Value");
+		}else{
+            int megas=kiloBytes/1024;
+            int KBrestantes=-((megas*1024)-kiloBytes);
+            System.out.println(kiloBytes+" KB = "+megas+" MB and "+KBrestantes+" KB");
+        }
     }
 }
