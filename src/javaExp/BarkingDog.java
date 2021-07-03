b9bca9ebcff2f6824b2337b5db386e815f922a20
magnitopic <magnitopic@gmail.com>
2021-07-03 10:37:43 +0200

    LeapYearCalculator

--- src/javaExp/BarkingDog.java
+++ src/javaExp/BarkingDog.java
@@ -5,7 +5,7 @@ public class BarkingDog {
         System.out.print( shouldWakeUp(true, 24));
     }
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
-        if ((barking == true) && ((hourOfDay < 8) || (hourOfDay > 22))&&(hourOfDay > 0) && (hourOfDay <= 23)) {
+        if ((barking == true) && ((hourOfDay < 8) || (hourOfDay > 22))&&(hourOfDay >= 0) && (hourOfDay <= 23)) {
         	return true;
         } else {
             return false;
---
+++ src/javaExp/LeapYearCalculator.java
@@ -0,0 +1,27 @@
+package javaExp;
+
+public class LeapYearCalculator {
+	public static void main(String[] args) {
+	    System.out.print( isLeapYear(1800));
+	}
+	
+	public static boolean isLeapYear(int year) {
+		if ((year>=1)&&(year<9999)) {
+			if((year%4==0)) {
+				if(year%100==0){
+				    if(year%400==0){
+				        return true;
+				    }else{
+				        return false;
+				    }
+				}else{
+				    return true;
+				}
+			}else {
+				return false;
+			}
+		}else {
+			return false;
+		}
+	}
+}
