c0b2d2f8341888943ed9321d51b41974c274ad58
magnitopic <magnitopic@gmail.com>
2021-07-04 14:00:57 +0200

    MinutesCalculator

---
+++ src/javaExp/MinutesCalculator.java
@@ -0,0 +1,16 @@
+package javaExp;
+
+public class MinutesCalculator {
+	public static void main(String[] args) {
+		printYearsAndDays(1051200);
+	}
+	public static void printYearsAndDays(long minutes) {
+		if (minutes<0) {
+			System.out.println("Invalid Value");
+		}else {
+			int years=(int)(minutes/525600);
+			int days=(int)((minutes%525600)/1440);
+			System.out.println(minutes+" min = "+years+" y and "+days+" d");
+		}
+	}
+}
