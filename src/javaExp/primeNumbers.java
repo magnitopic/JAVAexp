3728b0267d0007efbf6400bd96174d4d5d75f551
magnitopic <magnitopic@gmail.com>
2021-07-06 01:18:45 +0200

    primeNumbers

---
+++ src/javaExp/primeNumbers.java
@@ -0,0 +1,26 @@
+package javaExp;
+
+public class primeNumbers {
+    public static void main(String[] args) {
+        int counter=0;
+        for(int i=2;i<=1000;i++){
+            if (counter==3){
+                break;
+            }else if(isPrime(i)){
+                counter++;
+                System.out.println(i+" is a prime number. A total of "+counter+" prime numbers have been saved");
+            }
+        }
+    }
+    public static boolean isPrime(int n){
+       if (n<=1){
+           return false;
+       }
+       for (int i=2;i<=n/2;i++){
+        if(n%i==0){
+            return false;
+        }
+       }
+       return true;
+    }
+}
