616072044db27303c379d8361405b3ccd562bd55
magnitopic <magnitopic@gmail.com>
2021-07-05 13:34:26 +0200

    CalculateInterest

---
+++ src/javaExp/CalculateInterest.java
@@ -0,0 +1,14 @@
+package javaExp;
+
+public class CalculateInterest {
+    public static void main(String[] args) {
+        double amount=10000;
+        for (double i=2;i<=8;i++){
+            System.out.println(Math.round(calculateInterest(amount, i))+" € of interest at "+i+"% interest");
+        }
+    }
+    public static double calculateInterest(double amount,double interestRate){
+        return (amount *(interestRate/100));
+    }
+}
+
