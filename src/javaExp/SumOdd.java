d6ae486d32628188289663d4df08db1f289319b4
magnitopic <magnitopic@gmail.com>
2021-07-06 01:56:39 +0200

    SumOdd

---
+++ src/javaExp/SumOdd.java
@@ -0,0 +1,26 @@
+package javaExp;
+
+public class SumOdd {
+    public static int sumOdd(int start,int end){
+        if ((start<=end)&&(start>0)){
+            int sum=0;
+            for (int i=start;i<=end;i++){
+                if (isOdd(i)){
+                    sum=sum+i;
+                }
+            }
+            return sum;
+        }else{
+            return -1;
+        }
+
+    }
+
+    public static boolean isOdd(int number){
+        if((number<0)||(number%2==0)){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
