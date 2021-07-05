bfaec3a67525233c6a875b5d6ac5dc300cdbd26a
magnitopic <magnitopic@gmail.com>
2021-07-05 16:09:31 +0200

    SecretNumber memory leak

--- src/javaExp/CalculateInterest.java
+++ src/javaExp/CalculateInterest.java
@@ -3,12 +3,14 @@ package javaExp;
 public class CalculateInterest {
     public static void main(String[] args) {
         double amount=10000;
-        for (double i=2;i<=8;i++){
+        for (int i=2;i<=8;i++){
             System.out.println(String.format("%.2f",calculateInterest(amount, i))+" € of interest at "+i+"% interest");
+        }
+        for (int j=8;j>=10;j++){
+            System.out.println(String.format("%.2f",calculateInterest(amount, j))+" € of interest at "+j+"% interest");
         }
     }
     public static double calculateInterest(double amount,double interestRate){
         return (amount *(interestRate/100));
     }
 }
-
