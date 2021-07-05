b19febcb467c1980b74e4753b792fad25b48b2a0
magnitopic <magnitopic@gmail.com>
2021-07-05 16:13:08 +0200

    Revert "SecretNumber memory leak"
    This reverts commit bfaec3a67525233c6a875b5d6ac5dc300cdbd26a.

--- src/javaExp/CalculateInterest.java
+++ src/javaExp/CalculateInterest.java
@@ -3,14 +3,12 @@ package javaExp;
 public class CalculateInterest {
     public static void main(String[] args) {
         double amount=10000;
-        for (int i=2;i<=8;i++){
+        for (double i=2;i<=8;i++){
             System.out.println(String.format("%.2f",calculateInterest(amount, i))+" € of interest at "+i+"% interest");
-        }
-        for (int j=8;j>=10;j++){
-            System.out.println(String.format("%.2f",calculateInterest(amount, j))+" € of interest at "+j+"% interest");
         }
     }
     public static double calculateInterest(double amount,double interestRate){
         return (amount *(interestRate/100));
     }
 }
+
