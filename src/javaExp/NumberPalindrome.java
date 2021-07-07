88e609b8d16dd75b273490a03966e22900f2da4f
magnitopic <magnitopic@gmail.com>
2021-07-07 10:24:43 +0200

    shorter algorithm for isPalindrome

--- src/javaExp/NumberPalindrome.java
+++ src/javaExp/NumberPalindrome.java
@@ -1,16 +1,25 @@
 package javaExp;
+
 public class NumberPalindrome {
 	public static void main(String[] args) {
 		System.out.println(isPalindrome(-801108));
 	}
-	public static boolean isPalindrome(int number){
-		int reversed=0,number2=number;
-		for(;number != 0; number /= 10) {
+
+	public static boolean isPalindrome(int number) {
+		int reversed = 0, number2 = number;
+		for (; number != 0; number /= 10) {
 			int digit = number % 10;
 			reversed = reversed * 10 + digit;
 		}
-		if (reversed==number2){
-			
+		if (reversed == number2) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isPalindromeShort(int number) {
+		String reversed = new StringBuilder(Integer.toString(number)).reverse().toString();
+		if (Integer.parseInt(reversed) == number) {
 			return true;
 		}
 		return false;
