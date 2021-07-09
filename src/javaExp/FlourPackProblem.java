d871c46596e9d9dddd15cec140b69aead20f0c07
magnitopic <magnitopic@gmail.com>
2021-07-09 23:27:49 +0200

    FlourPackProblem

---
+++ src/javaExp/FlourPackProblem.java
@@ -0,0 +1,28 @@
+package javaExp;
+
+public class FlourPackProblem {
+	public static void main(String[] args) {
+		System.out.println(canPack(0, 5, 6));
+	}
+
+	public static boolean canPack(int bigCount, int smallCount, int goal) {
+		if (bigCount >= 0 && smallCount >= 0) {
+			int total = 0;
+			int diference = goal - total;
+			while ((diference / 5 != 0) && (bigCount > 0)) {
+				total = total + 5;
+				bigCount--;
+				diference = goal - total;
+			}
+			while ((diference != 0) && (smallCount > 0)) {
+				total = total + 1;
+				diference = goal - total;
+				smallCount--;
+			}
+			if (total == goal) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
