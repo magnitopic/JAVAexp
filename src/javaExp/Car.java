824086255f77645f9c8201073ac22acc46e1bc11
magnitopic <magnitopic@gmail.com>
2021-07-14 18:50:38 +0200

    First object in Java: Car

---
+++ src/javaExp/Car.java
@@ -0,0 +1,13 @@
+package javaExp;
+
+//Creating the usual car object in Java
+
+public class Car {
+	public int doors;
+	public int wheels;
+	public String model;
+
+	public void setModel(String model){
+		this.model= model;
+	}
+}
---
+++ src/javaExp/Objects.java
@@ -0,0 +1,8 @@
+package javaExp;
+
+public class Objects {
+	public static void main(String[] args) {
+		Car volswagen = new Car();
+		volswagen.setModel("Golf");
+	}
+}
